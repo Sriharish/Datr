070ed13370fb101fdbe152714811e0e291ea4141
Sriharish <sriharish.v@gmail.com>
2020-12-28 11:18:58 -0600

    modify User Entity; add DateTimeExtensions

--- Datr.API/Entities/AppUser.cs
+++ Datr.API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Datr.API.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +12,22 @@ namespace Datr.API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Handle { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; }
+
+        public string Summary { get; set; }
+        public string InterestedIn { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+        
+        public int GetAge()
+        {
+            return DateOfBirth.GetAge();
+        }
     }
 }
---
+++ Datr.API/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Datr.API.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int GetAge(this DateTime dateOfBirth)
+        {
+            DateTime now = DateTime.Now;
+            DateTime birthdayInCurrentYear = new DateTime(dateOfBirth.Year, dateOfBirth.Month, dateOfBirth.Day);
+            int age = now.Year - dateOfBirth.Year;
+
+            return now >= birthdayInCurrentYear ? age : age--;
+        }
+    }
+}
